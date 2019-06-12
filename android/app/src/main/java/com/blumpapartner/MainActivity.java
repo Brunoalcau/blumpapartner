f76d379fc5685525089533bc1cc761e5b0c40735
Bruno Caú <brunoalcau@gmail.com>
2019-06-12 11:52:04 -0300

    Erro no MainActivity

--- android/app/src/main/java/com/blumpapartner/MainActivity.java
+++ android/app/src/main/java/com/blumpapartner/MainActivity.java
@@ -3,9 +3,9 @@ package com.blumpapartner;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
-+ import com.facebook.react.ReactActivityDelegate;
-+ import com.facebook.react.ReactRootView;
-+ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
